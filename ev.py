10454dd626d4a5800777c8f50414e70ca0d4d335
Sreejita Ghosh <sreejita.ghosh@student.griffith.ie>
2020-03-15 00:42:24 +0000

    Starting Assignment

---
+++ ev.py
@@ -0,0 +1,53 @@
+import webapp2
+import jinja2
+from google.appengine.api import users
+from google.appengine.ext import ndb
+import os
+
+from user import MyUser
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname("E:\\GCD\\Sem 2\\Cloud Computing\\EV DATABASE\\")),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True
+)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+
+        url_string = ''
+        url = ''
+
+
+        user = users.get_current_user()
+
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_string = 'logout'
+
+            myuser_key = ndb.Key('MyUser', user.user_id())
+            myuser = myuser_key.get()
+
+            if myuser == None:
+                welcome = 'Welcome to the application'
+                myuser = MyUser(id=user.user_id())
+                myuser.put()
+
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_string = 'login'
+
+        template_values = {
+             'url' : url,
+             'url_string' : url_string,
+             'user' : user,
+
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('ev.html')
+        self.response.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
